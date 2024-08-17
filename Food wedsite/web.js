347ca1725203716984264fc754eed6dedfecf12e
Sazib Hossain <153016434+Sazib27@users.noreply.github.com>
2024-08-17 19:06:25 +0600

    My first committing

---
+++ Food wedsite/web.js
@@ -0,0 +1,7 @@
+//.......Toggle-light-dark-mode......//
+const themeSwitch = document.getElementById('theme-switch');
+const body =document.body;
+
+themeSwitch.addEventListener('change',function(){
+    body.classList.toggle('dark-mode');
+});
